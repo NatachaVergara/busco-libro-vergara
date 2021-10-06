9cc6c55f5148fecc77f4aea9c7679381f3c8b7bf
Natacha Vergara <ntchvergara@gmail.com>
2021-10-06 15:28:49 -0300

    Agregando h1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="H1">Segunda clase en REACT</h1>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
