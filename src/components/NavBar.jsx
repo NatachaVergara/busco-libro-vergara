48926f12a652c3a207341b8b02fdcb34c3e8d993
Natacha Vergara <ntchvergara@gmail.com>
2021-10-14 10:26:10 -0300

    jugando con el navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,41 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import { Container,Nav } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar'
+
+
 
 function NavBar() {
     return (
-        <Navbar bg="black" expand="lg">
-            <Container fluid>
-                <Navbar.Brand className="text-light" href="#">Busco Libro</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" className="bg-danger opacity-75" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0 ms-xl-5 ps-xl-5"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link className="text-light" href="#action1"><i class="bi bi-bag"></i>Comprar</Nav.Link>
-                        <Nav.Link className="text-light" href="#action2">Vender</Nav.Link>
-                    </Nav>
-                    <Nav className="justify-content-end me-xl-5" activeKey="/home">
-                        <Nav.Item>
-                            <Nav.Link className="text-light" href="/home">Login</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link className="text-light" eventKey="link-1">Register</Nav.Link>
-                        </Nav.Item>
-                        
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand ms-md-5" href="#">Busco Libro</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse d-xl-flex " id="navbarColor01">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Comprar
+                                <span className="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Vender</a>
+                        </li>                        
+                    </ul>
+                    <ul className="navbar-nav me-auto ms-md-5 ps-md-5">
+                        <li className="nav-item mb-2">
+                            <button type="button" class="btn btn-outline-dark">Login</button>
+                        </li>
+                        <li className="nav-item ms-md-2">
+                            <button type="button" class="btn btn-outline-dark">Sign in</button>
+                        </li>
+                    </ul>                  
+                </div>
+                           
+            </div>
+            
+        </nav>
     )
 }
-
 export default NavBar
