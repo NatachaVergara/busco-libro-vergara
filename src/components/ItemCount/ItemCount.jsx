dfa8c2f51cd7b1d834dc113239d5eeacb7ef0e6d
Natacha Vergara <ntchvergara@gmail.com>
2021-10-29 10:55:29 -0300

    Probando Modal

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './itemCount.css'
 
-const ItemCount = ({ stock, initial, onAdd}) => {
+const ItemCount = ({ stock, initial, onAdd }) => {
     // eslint-disable-next-line no-unused-vars
     const [count, setCount] = useState(1)
+    const [modal, setModal] = useState()
 
-   
+
     const addItem = () => {
-        if (count <= stock) {            
-           setCount(count + 1)           
-         }
+        if (count <= stock) {
+            setCount(count + 1)
+        }
     }
 
     const removeItem = () => {
@@ -17,14 +18,20 @@ const ItemCount = ({ stock, initial, onAdd}) => {
             setCount(count - 1)
         }
     }
-    
-    const btnAdd = () => {        
+
+    const btnAdd = () => {
         if (count >= 1) {
             onAdd()
-            
+
+        } else { useModal() }
+    }
+
+    const useModal = () => {
+        if (modal) {
+            setModal(!modal)
         }
     }
- 
+
 
     return (
         <div className="container text-center ">
@@ -35,7 +42,7 @@ const ItemCount = ({ stock, initial, onAdd}) => {
                 <button type="button" className="btn btn-primary btn-sm" onClick={addItem} > + </button>
             </div>
             <button type="button" className="btn btn-secondary" onClick={btnAdd}>Agregar al carrito</button>
-            
+
         </div>
     )
 }
