999e0ffc322fb91e1913c8c27c3a13f6ccbb217e
Natacha Vergara <ntchvergara@gmail.com>
2021-10-31 18:50:54 -0300

    clase 5 Components II Contador con boton

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -8,33 +8,21 @@ const ItemCount = ({ stock, initial, onAdd }) => {
     //Funcion para sumar items al carrito
     //mientras que el contador sea menor a stock, va a sumar
     const addItem = () => {
-        if (count < stock) {
-            setCount(count + 1)
-            stock = stock - (count + 1)
-            // console.log(`Queda en stock ${stock}`)
-        }
-
+        count >= stock ? console.log('No hay más stock para agregar') : setCount(count + 1)
+        
     }
     //Funcion para remover items del carrito
     //Mientras el count sea mayor a inicial, va a restar
     const removeItem = () => {
-        if (count >= initial) {
-            setCount(count - 1)
-            stock = stock - (count - 1)
-            // console.log(`Queda en stock ${stock}`)
-        }
-
+        count >= initial ? setCount(count - 1) : console.log(`No hay items para agregar`)
+        
     }
 
 
-    //Funcion que utiliza la funcion creada onAdd del itemListCointainer que me permite habilitar el btn Agregar al carrito
-    const btnAdd = () => {
-        //Si stock es mayor a 0 y ademas count es mayor o igual a initial puede accionarse el btn Agregar al carrito
-        //El problema que tengo es que no se como bloquearlo cuando ya agregué el último item y el stock queda en 0 e hice click 1 vez en el btn.
-        //me toma el stock inicial y no el que se va descontando *** no supe como manejar es aparte
-        if (stock && count >= initial) {
-            onAdd()
-        }
+  //  Funcion que utiliza la funcion creada onAdd del itemListCointainer que me permite habilitar el btn Agregar al carrito
+    const btnAdd = () => {       
+      onAdd()
+      
     }
 
 
@@ -42,13 +30,13 @@ const ItemCount = ({ stock, initial, onAdd }) => {
         <div className="container text-center ">
             <h6> El amuleto </h6>
             <div className="mb-3 d-flex flex-row justify-content-center align-items-center">
-                <button type="button" className="btn btn-primary btn-sm" onClick={addItem} > + </button>
+                <button type="button" className="btn btn-primary btn-sm" onClick={addItem}  > + </button>
                 <h2 className="mx-2">{count}</h2>
 
-                <button type="button" className="btn btn-primary btn-sm me-1" onClick={removeItem} > - </button>
+                <button type="button" className="btn btn-primary btn-sm me-1" onClick={removeItem}  > - </button>
             </div>
 
-            <button type="button" className="btn btn-secondary" onClick={btnAdd}>Agregar al carrito</button>
+            <button type="button" className="btn btn-secondary" onClick={btnAdd} disabled={count < initial}>Agregar al carrito</button>
         </div>
     )
 }
