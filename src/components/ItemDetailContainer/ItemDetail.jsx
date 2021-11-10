28b20de71afbc679bd7dce8b941279ea52662b4a
Natacha Vergara <ntchvergara@gmail.com>
2021-11-10 19:38:34 -0300

    Primera Entrega del Proyecto Final

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 import ItemCount from "../ItemCount/ItemCount"
 
 const ItemDetail = ({ item }) => {
-    
+
 
     const onAdd = () => {
-    console.log('Agregado')
-}
+        console.log('Agregado')
+    }
     return (
-        
+
         <div className="animate__animated animate__fadeInDown container d-flex justify-content-center" >
-            <div className="card mt-2 mb-2 " style={{ width: "30rem" }} >
-                <h3 className="card-title m-2 text-center fs-3 text-dark">{item.title}  </h3>
-                <h4 className="text-center text-dark">Autor: {item.autor} </h4>
-                <img src={item.pictureUrl} className="card-img-top container" style={{ width: "250px" }} alt="img" />
+            <div className="card mt-2 mb-2 d-flex flex-row" >
+                <div className="mt-md-5 mt-lg-3" >
+                    <h6 className="text-center text-dark ms-2 mb-3"> {item.autor} </h6>
+                    <img src={item.pictureUrl} className="card-img-top container" style={{ width: "150px", height: "150px" }} alt="img" />
+                </div>
+
                 <div className="card-body text-center">
-                    <h4 className="text-center">Sinopsis</h4>
+                    <h1 className="card-title ms-2 mb-3 text-center text-dark"> {item.title}  </h1>
+                    <h4 className="text-center" >Sinopsis</h4>
                     <p className="card-text text-dark"> {item.resumen}  </p>
+                    <ul className="list-group list-group-flush w-xl-25 h-auto">
+                        <li className="list-group-item text-dark fs-5">Stock disponible: {item.stock} unidades  </li>
+                        <li className="list-group-item text-dark fs-5">Estado: {item.estado} </li>
+                        <li className="list-group-item text-dark fs-5">Precio: ${item.price} </li>
+                    </ul>
                 </div>
-                <ul className="list-group list-group-flush text-center">
-                    <li className="list-group-item text-dark fs-5">Stock disponible: {item.stock} unidades  </li>
-                    <li className="list-group-item text-dark fs-5">Estado: {item.estado} </li>
-                    <li className="list-group-item text-dark fs-5">Precio: ${item.price} </li>
-                </ul>
-                <div className ="card-body">
-                   <ItemCount stock={item.stock} initial="1" title={item.title} onAdd={onAdd} />
+
+                <div className="card-body mt-5">
+                    <ItemCount stock={item.stock} initial="1" title={item.title} onAdd={onAdd} />
                 </div>
-            </div>           
+            </div>
+
         </div>
     )
 }
--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -10,7 +10,7 @@ const Item = ({ id, title, price, pictureUrl }) => {
         <div className="animate__animated animate__zoomIn col-sm-12 col-md-6 col-lg-4" id={id}>
                 <div className="card mb-3">
                     <p className="card-header text-dark">{title} </p>
-                    <img src={pictureUrl} className="card-img-top container mb-3 mt-2" style={{ width: "250px", height: "300px" }} alt="..." />
+                    <img src={pictureUrl} className="card-img-top container mb-3 mt-2" style={{ width: "250px", height: "250px" }} alt="..." />
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item text-dark">Precio: $ {price} </li>
                     </ul>
