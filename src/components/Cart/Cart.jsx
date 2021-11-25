14f8d3851f803eea9972915659b570af4256eda1
Natacha Vergara <ntchvergara@gmail.com>
2021-11-25 18:22:06 -0300

    desafio 12 casi terminado

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,41 +1,81 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
 import '../../index.css'
 import { CartEmpty } from './CartEmpty'
+import firebase from 'firebase'
 import { getFirestore } from '../../service/fireBaseConfig'
 
 export const Cart = () => {
-    //   const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({})
 
     const { cartList, eraseCart, eraseItem, cantItem, totalPrice } = useCartContext()
-  
+
     const generarOrden = (e) => {
         e.preventDefault();
         const order = {}
 
-        order.buyer = { name: 'Natacha', email: 'natacha@gmail.com', tel: '44418770' }
-        order.total = totalPrice()
 
+        order.buyer = formData
+        order.total = totalPrice()
+        order.date = firebase.firestore.Timestamp.fromDate(new Date())
         order.items = cartList.map(cartItem => {
             const id = cartItem.id
-            const title = cartItem.title.title
+            const title = cartItem.title
             const price = cartItem.price * cartItem.cantidad
             return { id, title, price }
         })
 
 
+
         const dbQ = getFirestore()
-        dbQ.collection('orders').add(order)
-            .then(resp => console.log(resp))
+        const orders = dbQ.collection('orders')
+
+        //Creo una colleccion llamada orders donde se va a ir guardando cada orden de compra realizada
+        orders.add(order)
+            .then(resp => console.log(`Su orden fue procesada, su numero de id es: ${resp.id}`))
             .catch(err => console.log(err))
 
+        //Actualiza todos los items que estan en el listado de Cart del cartContext
+        const itemsToUpdate = dbQ.collection('items').where(firebase.firestore.FieldPath.documentId(), 'in', cartList.map(i => i.id))
 
+
+        const batch = dbQ.batch();
+        console.log(batch)
+
+        //Por cada item, restarl del stockla cantidad de el carrito
+        itemsToUpdate.get()
+            .then(collection => {
+                collection.docs.forEach(docSnapshot => {
+                    batch.update(docSnapshot.ref, {
+                        stock: docSnapshot.data().stock - cartList.find(item => item.id === docSnapshot.id).cantidad
+                    })
+                })
+                batch.commit().then(res => {
+                    console.log(`Stock actualizado`)
+                })
+
+            })
         console.log(order)
-
+        eraseCart()
     }
 
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.nombre]: e.target.value,
+            [e.target.apellido]: e.target.value,
+            [e.target.email]: e.target.value,
+            [e.target.tel]: e.target.value,
+            [e.target.direccion]: e.target.value,
+            [e.target.codigoPostal]: e.target.value
+
+        })
+    }
+    console.log(formData)
 
     return (
         <div>
@@ -75,48 +115,49 @@ export const Cart = () => {
                 cartList.length === 0 ? null :
                     <>
                         <button type="button" className="btn btn-outline-secondary ms-5 " onClick={eraseCart}>Eliminar carrito</button>
-                        <Link to='/' className="card-link m-5"><button type="button" className="btn btn-outline-danger ms-5 ">  Seguir comprando </button></Link>
+                        <Link to='/' className="card-link m-5">
+                            <button type="button" className="btn btn-outline-danger ms-5 ">  Seguir comprando </button>
+                        </Link>
                         <div className="card container vw-100 mt-3 mb-5 text-center ">
                             <div className="card-body">
                                 <h4 className="card-title">Comprar Carrito</h4>
                                 <h6 className="card-subtitle mb-2 text-muted">Cantidad de productos: {cantItem()} </h6>
                                 <p className="card-text">Total: {totalPrice()}  </p>
 
-                                <form onSubmit={generarOrden}>
-                                    {/* <div class="col-md-6">
-                                        <label for="nombre" class="form-label">Nombre</label>
-                                        <input type="text" class="form-control" id="nombre"/>
+
+                                <form onSubmit={generarOrden}
+                                    onchange={handleChange}
+                                    className="container">
+                                    <div class="col-md-6">
+                                        <label for="nombre" className="form-label">Nombre</label>
+                                        <input type="text" value={formData.nombre} className="form-control" id="nombre" />
                                     </div>
                                     <div class="col-md-6">
-                                        <label for="apellido" class="form-label">Apellido</label>
-                                        <input type="text" class="form-control" id="apellido"/>
+                                        <label for="apellido" className="form-label">Apellido</label>
+                                        <input type="text" value={formData.apellido} className="form-control" id="apellido" />
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="email" className="form-label">Email</label>
+                                        <input type="email" value={formData.email} className="form-control" id="email" />
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="tel" className="form-label">Teléfono</label>
+                                        <input type="number" value={formData.tel} className="form-control" id="tel" />
                                     </div>
                                     <div class="col-12">
-                                        <label for="inputAddress" class="form-label">Direccion</label>
-                                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                                        <label for="inputAddress" className="form-label">Direccion</label>
+                                        <input type="text" value={formData.direccion} className="form-control" id="inputAddress" placeholder="1234 Main St" />
                                     </div>
-                                    <div class="col-md-6">
-                                        <label for="inputEmail4" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="inputEmail4" />
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="inputCity" class="form-label">Ciudad</label>
-                                        <input type="text" class="form-control" id="inputCity"/>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="inputState" class="form-label">Provincia</label>
-                                        <select id="inputState" class="form-select">
-                                            <option selected>Choose...</option>
-                                            <option>...</option>
-                                        </select>
-                                    </div>
+
                                     <div class="col-md-2">
-                                        <label for="inputZip" class="form-label">Zip</label>
-                                        <input type="text" class="form-control" id="inputZip"/>
-                                    </div> */}
-                                    
+                                        <label for="codigoPostal" className="form-label">Código Postal</label>
+                                        <input type="number" value={formData.codigoPostal} className="form-control" id="codigoPostal" />
+                                    </div>
+
                                     <button className="card-link" >Terminar compra</button>
+
                                 </form>
+
 
                             </div>
                         </div>
