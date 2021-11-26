c2f15010182e800ff6ec92ee6cb544609624dd98
Natacha Vergara <ntchvergara@gmail.com>
2021-11-26 15:07:36 -0300

    Compra finalizada con éxito

--- src/components/ModalCompra/CompraFinalizada.jsx
+++ src/components/ModalCompra/CompraFinalizada.jsx
@@ -13,7 +13,11 @@ const CompraFinalizada = (props) => {
                 <Modal.Header closeButton>
                     <Modal.Title>Compra finalizada con éxito</Modal.Title>
                 </Modal.Header>
-                <Modal.Body> `El id de su orden es: {props.orderId} y el total es ${props.total}` </Modal.Body>
+                <Modal.Body className="text-success fs-5 bg-dark"> El id de su orden es: {props.orderId} y el total es ${props.total} <br />
+                    El producto sera enviado en las proximas 48hrs.
+                    ¡Gracias por comprar con nosotros!</Modal.Body>
+                
+               
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.onHide}>
                         Close
