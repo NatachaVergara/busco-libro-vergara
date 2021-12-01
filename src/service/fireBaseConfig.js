478a9060734b426c6b73f7a8fa6e8dcd80233381
Natacha Vergara <ntchvergara@gmail.com>
2021-12-01 14:08:26 -0300

    no se actualiza netlify

--- src/service/fireBaseConfig.js
+++ src/service/fireBaseConfig.js
@@ -3,15 +3,6 @@ import firebase  from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    // --> cuando haga el deploid voy a usar el env.local
-    // apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    // appId: process.env.REACT_APP_FIREBASE_APPID,
-    // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
-
     apiKey: "AIzaSyA7d6JaRAjWG3qQbMS0w6K7M5Nb5jhbSK4",
     authDomain: "busco-libro-app.firebaseapp.com",
     projectId: "busco-libro-app",
