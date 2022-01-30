f079ce48e376cd952c975379a49ec7461f39a22f
Natacha Vergara <ntchvergara@gmail.com>
2022-01-30 00:20:54 -0300

    arregle un bug que tenia mi itemcount, me agregaba un item y sumaba al carrito aun fuera de stock y agregue el monto total en el cartwitget

--- src/utils/createUser.jsx
+++ src/utils/createUser.jsx
@@ -7,10 +7,6 @@ const createUser = async (registerEmail, registerPassword, setShowModal) => {
     try {
         await auth.createUserWithEmailAndPassword(registerEmail, registerPassword)
         setShowModal(true)
-<<<<<<< HEAD
-
-=======
->>>>>>> cd818ae875ab6072b3f10dbd6f92a83855c1db9f
     } catch (error) {
         switch (error.code) {
             case "auth/email-already-in-use":
@@ -20,12 +16,6 @@ const createUser = async (registerEmail, registerPassword, setShowModal) => {
                 alert("Algo salió mal, intenta recargando la página");
                 break;
         }
-<<<<<<< HEAD
-=======
-
-    }
-
->>>>>>> cd818ae875ab6072b3f10dbd6f92a83855c1db9f
 
     }
 
